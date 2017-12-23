dca1f9f12f5292396ddeda00f4e0fb314520d299
JeffersonYeung <jeffyccheong@hotmail.com>
2017-12-23 10:59:31 +0800

    Commit calendar view and day view

--- app/src/main/java/com/msccs/hku/familycaregiver/Activity/CalendarActivity.java
+++ app/src/main/java/com/msccs/hku/familycaregiver/Activity/CalendarActivity.java
@@ -1,15 +1,20 @@
 package com.msccs.hku.familycaregiver.Activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.msccs.hku.familycaregiver.R;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,8 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +33,18 @@ public class CalendarActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        calendar = (CalendarView) findViewById(R.id.calendarView); // get the reference of CalendarView
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + (month + 1) + "/" + year, Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+
+
     }
 
 }
